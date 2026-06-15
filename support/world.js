4b51060531f8245d4f35fc176c0d41361a89a85b
Drishti-CitiusTech <97043179+Drishti-CitiusTech@users.noreply.github.com>
2026-06-15 19:30:48 +0530

    Pass selected environment to test execution

--- support/world.js
+++ support/world.js
@@ -1,14 +1,13 @@
 const { setWorldConstructor, World } = require('@cucumber/cucumber');
 const env = process.env.TEST_ENV || 'qa'; // Default to 'qa' if TEST_ENV is not set
 
-require ('dotenv').config({ path: `config/${env.toLowerCase()}.env` });
+require ('dotenv').config({ path: `config/${env}.env` });
 
 /** * Custom World for Cucumber tests to share context between steps
  */
 class CustomWorld extends World {
     constructor(options) {
         super(options);
-
         this.browser = null;
         this.context = null;
         this.page = null;
