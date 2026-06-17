3fb94b3f8c08a0bf3f0a9c3d084704742af0c355
Drishti-CitiusTech <97043179+Drishti-CitiusTech@users.noreply.github.com>
2026-06-17 14:40:23 +0530

    Fixed dynamic env metadata in report

--- generateReport.js
+++ generateReport.js
@@ -2,6 +2,9 @@ const reporter = require('multiple-cucumber-html-reporter');
 const env = process.env.TEST_ENV || 'qa'; // Default to 'qa' if TEST_ENV is not set
 require('dotenv').config({ path: `config/${env}.env` });
 
+const selectedEnv = process.env.TEST_ENV || 'qa';
+const environmentName = selectedEnv.toUpperCase();
+
 reporter.generate({
   jsonDir: 'reports',
   reportPath: 'reports/html-report',
@@ -22,7 +25,7 @@ reporter.generate({
       { label: 'Project', value: 'Checkout Flow of Sauce Labs E-Commerce' },
       { label: 'Framework', value: 'Playwright JavaScript with Cucumber BDD' },
       { label: 'Tester', value: 'Drishti Kashyap' },
-      { label: 'Environment', value: process.env.ENVIRONMENT || 'QA' },
+      { label: 'Environment', value: environmentName || 'QA' },
       { label: 'Browser', value: process.env.BROWSER || 'Chromium' },
       { label: 'Execution Type', value: process.env.CI ? 'GitHub Actions CI' : 'Local Execution' },
       { label: 'Execution Date', value: new Date().toLocaleString() }
